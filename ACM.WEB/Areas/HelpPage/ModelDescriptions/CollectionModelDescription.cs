fd499f7aec4ce4f26061d9f9cde7069eddb8cfa8
Suz <suz.lynn.oliver@gmail.com>
2018-11-09 17:43:45 -0600

    Add project files.

---
+++ ACM.WEB/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace ACM.WEB.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
---
+++ ACM.WEB/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
@@ -0,0 +1,6 @@
+namespace ACM.WEB.Areas.HelpPage.ModelDescriptions
+{
+    public class DictionaryModelDescription : KeyValuePairModelDescription
+    {
+    }
+}
